beea9762788092725b55c40317fbd8c33fe8cd87
Jonathan Vacek <jonathanjames1941@outlook.com>
2026-04-06 10:11:03 -0500

    update buid.sh for python module

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #/usr/bin/bash
 
-rm ./package/usr/bin/JLMP.py
-cp JLMP.py ./package/usr/bin/JLMP.py
+rm ./package/usr/lib/python3/dist-packages/JLMP.py
+cp JLMP.py ./package/usr/lib/python3/dist-packages/JLMP.py
 
 dpkg-deb --build package
